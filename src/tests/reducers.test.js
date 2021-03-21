5e10322d0de876d671dc7435b13ec3079e2b2dfe
neverovskii <neverovskii.na@gmail.com>
2021-03-21 21:41:55 +0300

    added new tests to reducers.test.js

--- src/tests/reducers.test.js
+++ src/tests/reducers.test.js
@@ -14,12 +14,20 @@ import {
     LOGOUT,
     TOKEN_EXPIRED,
     CREATE_CHAT,
-    GET_CHATS
+    GET_CHATS,
+    GET_MESSAGES,
+    SEND_MESSAGE,
+    CLEAR_MESSAGES,
+    RECEIVE_MESSAGE,
+    SET_POSTS,
+    SEND_POST,
+    CLEAR_POSTS,
+    SET_SEARCH_FIELD, SET_TOPIC_ID, FETCH_THREADS, FETCH_THREAD, CLEAR_THREAD
 } from "../redux/reducers/types";
 
 describe('adminReducer testing', () => {
 
-    let users = [{username: 'danila', user_id: 1}, {username: 'nikita', user_id: 2}, {username: 'misha', user_id: 3}, {username: 'vlad', user_id: 4}]
+    const users = [{username: 'danila', user_id: 1}, {username: 'nikita', user_id: 2}, {username: 'misha', user_id: 3}, {username: 'vlad', user_id: 4}]
 
     test('adminReducer initial state test', () => {
         expect(adminReducer(undefined, {})).toEqual({users: []})
@@ -30,7 +38,7 @@ describe('adminReducer testing', () => {
     })
 
     test('adminReducer DELETE_USERS action test', () => {
-        let deleted_id = 1
+        const deleted_id = 1
         expect(adminReducer({users : users}, {type: DELETE_USER, user_id: deleted_id})).toEqual({users: [{username: 'nikita', user_id: 2}, {username: 'misha', user_id: 3}, {username: 'vlad', user_id: 4}] })
     })
 
@@ -67,7 +75,6 @@ describe('authReducer testing', () => {
 
 describe('chatReducer testing', () => {
 
-
     test('chatReducer initial state test', () => {
         expect(chatReducer(undefined, {})).toEqual({
             chats: [],
@@ -77,7 +84,7 @@ describe('chatReducer testing', () => {
     })
 
     test('chatReducer CREATE_CHAT action test', () => {
-        let response = {chat_id: 1, participants: [1, 2]}
+        const response = {chat_id: 1, participants: [1, 2]}
         expect(chatReducer(undefined, {
             type: CREATE_CHAT,
             chat_id: response.chat_id,
@@ -85,27 +92,146 @@ describe('chatReducer testing', () => {
         })).toEqual({chats: [{chat_id: 1, participants: [1, 2]}], activeChat: '', messages: []})
     })
 
+    test('chatReducer GET_CHATS action test', () => {
+        const response = [{chat_id: 1, participants: [1, 2]}, {chat_id: 2, participants: [2, 3]}, {chat_id: 3, participants: [3, 4]}]
+        expect(chatReducer(undefined, {type: GET_CHATS, chats: response})).toEqual({chats: [{chat_id: 1, participants: [1, 2]}, {chat_id: 2, participants: [2, 3]}, {chat_id: 3, participants: [3, 4]}], activeChat: '', messages: []})
+    })
+
+    test('chatReducer GET_MESSAGES action test', () => {
+        const response = [{message_id: 1, chat_id: 1, body: 'test1'}, {message_id: 2, chat_id: 1, body: 'test2'}, {message_id: 3, chat_id: 2, body: 'test3'}]
+        expect(chatReducer(undefined, {type: GET_MESSAGES, messages: response})).toEqual({
+            chats: [],
+            activeChat: '',
+            messages: [{message_id: 1, chat_id: 1, body: 'test1'}, {message_id: 2, chat_id: 1, body: 'test2'}, {message_id: 3, chat_id: 2, body: 'test3'}],
+        })
+    })
+
+    test('chatReducer SEND_MESSAGE action test', () => {
+        const response = {message_id: 1, chat_id: 1, body: 'test1'}
+        const initialState = {
+            chats: [],
+            activeChat: '',
+            messages: [{message_id: 1, chat_id: 1, body: 'test1'}],
+        }
+        expect(chatReducer(initialState, {type: SEND_MESSAGE, message: response})).toEqual({
+            chats: [],
+            activeChat: '',
+            messages: [{message_id: 1, chat_id: 1, body: 'test1'}],
+        })
+    })
+
+    test('chatReducer CLEAR_MESSAGES action test', () => {
+        const initialState = {
+            chats: [{chat_id: 1, participants: [1, 2]}, {chat_id: 2, participants: [2, 3]}, {chat_id: 3, participants: [3, 4]}],
+            activeChat: '',
+            messages: [{message_id: 1, chat_id: 1, body: 'test1'}, {message_id: 2, chat_id: 1, body: 'test2'}, {message_id: 3, chat_id: 2, body: 'test3'}],
+        }
+        expect(chatReducer(initialState, {type: CLEAR_MESSAGES})).toEqual({
+            chats: [{chat_id: 1, participants: [1, 2]}, {chat_id: 2, participants: [2, 3]}, {chat_id: 3, participants: [3, 4]}],
+            activeChat: '',
+            messages: [],
+        })
+    })
+
+    test('chatReducer RECEIVE_MESSAGE action test', () => {
+        const message = {message_id: 2, chat_id: 1, body: 'test2'}
+        const initialState = {
+            chats: [],
+            activeChat: '',
+            messages: [{message_id: 1, chat_id: 1, body: 'test1'}, {message_id: 3, chat_id: 2, body: 'test3'}],
+        }
+        expect(chatReducer(initialState, {type:RECEIVE_MESSAGE, message: message})).toEqual({
+            chats: [],
+            activeChat: '',
+            messages: [{message_id: 1, chat_id: 1, body: 'test1'}, {message_id: 3, chat_id: 2, body: 'test3'}, {message_id: 2, chat_id: 1, body: 'test2'}],
+        })
+    })
+
 })
 
+describe('postsReducer testing', () => {
 
-test('postReducer initial state test', () => {
-    expect(postReducer(undefined, {})).toEqual({
-        posts: [],
+    test('postReducer initial state test', () => {
+        expect(postReducer(undefined, {})).toEqual({
+            posts: [],
+        })
+    })
+
+    test('postReducer SET_POSTS action test', () => {
+        const posts = [{post_id: 1, body: 'test1'}, {post_id: 2, body: 'test2'}, {post_id: 3, body: 'test3'}]
+        expect(postReducer(undefined, {type: SET_POSTS, posts: posts})).toEqual({posts: posts})
+    })
+
+    test('postReducer SEND_POST action test', () => {
+        const initialState = {posts: [{post_id: 1, responses: [], body: 'test1'}, {post_id: 2, responses: [], body: 'test2'}, {post_id: 3, responses: [], body: 'test3'}]}
+        const post = {post_id: 4, responses: [], body: 'test4'}
+        const parentId = 1
+        expect(postReducer(initialState, {type: SEND_POST, post: post, parentId: parentId})).toEqual({posts: [
+                {post_id: 1, responses: [{post_id: 4, responses: [], body: 'test4'}], body: 'test1'},
+                {post_id: 2, responses: [], body: 'test2'},
+                {post_id: 3, responses: [], body: 'test3'}]})
+    })
+
+    test('postReducer CLEAR_POSTS action test', () => {
+        const initialState = {posts: [{post_id: 1, responses: [], body: 'test1'}, {post_id: 2, responses: [], body: 'test2'}, {post_id: 3, responses: [], body: 'test3'}]}
+        expect(postReducer(initialState, {type: CLEAR_POSTS})).toEqual({posts: []})
     })
 })
 
-test('searchReducer initial state test', () => {
-    expect(searchReducer(undefined, {})).toEqual({
-        field: '',
-        topicId: '',
+describe('searchReducer testing', () => {
+
+    test('searchReducer initial state test', () => {
+        expect(searchReducer(undefined, {})).toEqual({
+            field: '',
+            topicId: '',
+        })
+    })
+
+    test('searchReducer SET_SEARCH_FIELD action test',  () => {
+        const field = 'search field'
+        expect(searchReducer(undefined, {type: SET_SEARCH_FIELD, field: field})).toEqual({
+            field: 'search field',
+            topicId: '',
+        })
+    })
+
+    test('searchReducer SET_TOPIC_ID action test', () => {
+        const id = 123
+        expect(searchReducer(undefined, {type: SET_TOPIC_ID, topicId: id})).toEqual({
+            field: '',
+            topicId: 123,
+        })
     })
 })
 
-test('threadReducer initial state test', () => {
-    expect(threadReducer(undefined, {})).toEqual({
-        threads: []
+describe('threadReducer testing', () => {
+
+    test('threadReducer initial state test', () => {
+        expect(threadReducer(undefined, {})).toEqual({
+            threads: []
+        })
     })
+
+    test('threadReducer FETCH_THREADS action test', () => {
+        const threads = [{thread_id: 1, body: 'test1'}, {thread_id: 2, body: 'test2'}, {thread_id: 3, body: 'test3'}]
+        expect(threadReducer(undefined, {type: FETCH_THREADS, threads: threads})).toEqual({threads: [{thread_id: 1, body: 'test1'},
+                {thread_id: 2, body: 'test2'},
+                {thread_id: 3, body: 'test3'}]})
+    })
+
+    test('threadReducer FETCH_THREAD action test', () => {
+        const thread = {thread_id: 1, body: 'test1'}
+        expect(threadReducer(undefined, {type: FETCH_THREAD, thread: thread})).toEqual({threads: [], thread: {thread_id: 1, body: 'test1'}})
+    })
+
+    test('threadReducer CLEAR_THREAD action test', () => {
+        const initialState = {threads: [{thread_id: 1, body: 'test1'}, {thread_id: 2, body: 'test2'}, {thread_id: 3, body: 'test3'}]}
+        expect(threadReducer(initialState, {type: CLEAR_THREAD, threads: ''})).toEqual({threads: ''})
+    })
+
+
 })
+
 
 test('topicReducer initial state', () => {
     expect(topicReducer(undefined, {})).toEqual({
