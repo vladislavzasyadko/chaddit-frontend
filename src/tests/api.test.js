ac68690f2b306a3c221c865e0fbd7ec674bc60c1
neverovskii <neverovskii.na@gmail.com>
2021-03-21 15:26:47 +0300

    api.test.js changes

--- src/tests/api.test.js
+++ src/tests/api.test.js
@@ -1,5 +1,12 @@
-import {chatAPI, threadAPI, topicAPI} from "../api/api";
+import {adminAPI, chatAPI, threadAPI, topicAPI} from "../api/api";
+import axios from "axios";
+import {BASE_URL} from "../CONSTANTS/API_CONSTANTS";
 
+let instance = axios.create({
+    baseURL: BASE_URL,
+})
+const api_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MTY0MTMyNTEsImlhdCI6MTYxNjMyNjg1MSwic3ViIjozfQ.YmemEzASkW-dDb-1FL4KeGpBFu10zH3-5_uz8VyGpWo'
+let chat_id_for_test
 
 test('getThreads() request/response test', () =>{
     return threadAPI.getThreads().then(data => {
@@ -77,6 +84,18 @@ test('getTopics() request/response test', () => {
 })
 
 test('getChats() request/response test', () => {
+
+    jest.mock("../api/api",() => jest.fn())
+    chatAPI.getChats = jest.fn(() => {
+        return instance.get(`chaddit/c/chats`, {
+            headers: {
+                'api_token': api_token,
+            }
+        }).then(response => {
+            return response.data
+        })
+    })
+
     return chatAPI.getChats().then(data => {
         data.forEach((chat) => {
             expect(chat).toHaveProperty('active', true)
@@ -98,6 +117,92 @@ test('getChats() request/response test', () => {
 
             expect(chat).toHaveProperty('updated_at')
             expect(typeof chat.updated_at).toBe('string')
+        })
+        chat_id_for_test = data[0].chat_id
+    })
+})
+
+test('getMessages() request/response test', () => {
+
+    jest.mock("../api/api",() => jest.fn())
+    chatAPI.getMessages = jest.fn((chat_id) => {
+        return instance.get(`chaddit/c/messages/${chat_id}`, {
+            headers: {
+                'api_token': api_token,
+            },
+            params: {
+                limit: -1
+            }
+        }).then(response => {
+            return response.data
+        })
+    })
+
+    return chatAPI.getMessages(chat_id_for_test).then(data => {
+        data.forEach((message) => {
+            expect(message).toHaveProperty('active', true)
+
+            expect(message).toHaveProperty('author_id')
+            expect(typeof message.author_id).toBe('number')
+
+            expect(message).toHaveProperty('body')
+            expect(typeof message.body).toBe('string')
+
+            expect(message).toHaveProperty('chat_id')
+            expect(typeof message.chat_id).toBe('number')
+
+            expect(message).toHaveProperty('created_at')
+            expect(typeof message.created_at).toBe('string')
+
+            expect(message).toHaveProperty('message_id')
+            expect(typeof message.message_id).toBe('number')
+
+            expect(message).toHaveProperty('updated_at')
+            expect(typeof message.updated_at).toBe('string')
+        })
+    })
+})
+
+test('getUsers() request response test', () => {
+
+    jest.mock("../api/api",() => jest.fn())
+    adminAPI.getUsers = jest.fn((chat_id) => {
+        return instance.get('chaddit/c/users', {
+            headers: {
+                'api_token': api_token,
+            }
+        }).then(response => {
+            return response.data
+        })
+    })
+
+    return adminAPI.getUsers().then(data => {
+        data.forEach((user) => {
+            expect(user).toHaveProperty('active', true)
+
+            expect(user).toHaveProperty('created_at')
+            expect(typeof user.created_at).toBe('string')
+
+            expect(user).toHaveProperty('role')
+            expect(typeof user.role).toBe('object')
+
+            expect(user).toHaveProperty('role_id')
+            expect(typeof user.role_id).toBe('number')
+
+            expect(user).toHaveProperty('updated_at')
+            expect(typeof user.updated_at).toBe('string')
+
+            expect(user).toHaveProperty('user_email')
+            expect(typeof user.user_email).toBe('string')
+
+            expect(user).toHaveProperty('user_id')
+            expect(typeof user.user_id).toBe('number')
+
+            expect(user).toHaveProperty('user_name')
+            expect(typeof user.user_name).toBe('string')
+
+            expect(user).toHaveProperty('user_tag')
+            expect(typeof user.created_at).toBe('string')
         })
     })
 })
