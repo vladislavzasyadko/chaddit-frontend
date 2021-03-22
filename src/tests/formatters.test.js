b76cd9d51979be6f14129361db6e2ec0310260f9
neverovskii <neverovskii.na@gmail.com>
2021-03-22 15:23:48 +0300

    added formatters.test.js

---
+++ src/tests/formatters.test.js
@@ -0,0 +1,17 @@
+import {colorIsLight, formatDate, formatReplyString} from "../utils/formatters";
+
+test('formatDate testing', () => {
+ const dateString = '2021-02-18T19:30:04.109082'
+ expect(formatDate(dateString)).toBe('19:30 18.02.2021')
+})
+
+test('formatReplyString testing', () => {
+ const reply = 'hello, this is mock reply for testing'
+ const len = 17
+ expect(formatReplyString(reply, len)).toBe('hello, this is mo...')
+})
+
+test('colorIsLight testing', () => {
+ const rgb = {r: 200, g: 100, b: 200}
+ expect(colorIsLight(rgb.r, rgb.g, rgb.b)).toBeTruthy()
+})
