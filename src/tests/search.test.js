c2255e8b53334046d63c3fd2b11cb4ae3aec0c31
neverovskii <neverovskii.na@gmail.com>
2021-03-24 19:45:26 +0300

    adminTopic.test.js, search.test.js

---
+++ src/tests/search.test.js
@@ -0,0 +1,38 @@
+import ReactDOM, {render, unmountComponentAtNode} from "react-dom";
+import {store} from "../redux/store";
+import {SET_SEARCH_FIELD, SET_USER} from "../redux/reducers/types";
+import {act} from "@testing-library/react";
+import {Provider} from "react-redux";
+import Search from "../components/Header/Search/Search";
+import React from "react";
+
+describe('Search component testing ', () => {
+    let container = null;
+    let mockStore
+    const mockProps = {
+        field: 'TOPICS',
+        topicId: ''
+    }
+
+    beforeEach(() => {
+        container = document.createElement("div");
+        document.body.appendChild(container);
+        mockStore = store
+        store.dispatch({type: SET_SEARCH_FIELD, field: 'TOPICS'})
+    })
+
+    afterEach(() => {
+        unmountComponentAtNode(container);
+        container.remove();
+        container = null;
+    });
+
+    test('Search component rendering', () => {
+        act(() => {
+            render(<Provider store={mockStore}><Search {...mockProps}/></Provider>, container)
+        })
+        console.log(container.innerHTML)
+        expect(container.querySelector('.searchInput').getAttribute('placeholder')).toBe('Что я могу для Вас найти? ' + mockStore.getState().search.field)
+    })
+
+})
